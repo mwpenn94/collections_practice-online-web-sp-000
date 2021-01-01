3e39cda2aa73966b35460e0abd2e017f3c684a70
Michael Penn <mwpenn94@gmail.com>
2021-01-01 03:59:18 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,35 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort {|a, b| b <=> a}
+end
+
+def sort_array_char_count(array)
+  array.sort {|a, b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+  array[1], array[2]
+end
+
+def reverse_array(array)
+   array.sort {|a, b| b <=> a}
+end
+
+#def kesha_maker(array)
+#  array.each.str(2) = "$"
+#end
+
+def find_a(array)
+  array.each.start_with?("a")
+end
+
+def sum_array(array)
+  return array.sum
+end
+
+def add_s(array)
+  array.drop(1){|word| word >> "s"}
+end
