7d6dec55203f66f6ece14be12ba994c5f7854cc7
Michael Penn <mwpenn94@gmail.com>
2021-01-01 07:23:40 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -31,7 +31,7 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.collect {|word| word + "s"}
-  array
-  array[1].chop
+  array_new = array.collect {|word| word + "s"}
+  array_new[1].chop
+  return array_new
 end
